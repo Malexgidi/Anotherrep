b7cad9cb65f99bb82f6c3b328447e51b3b222ccc
Malexgidi <alagbadamalik@gmail.com>
2023-06-18 05:58:01 -0400

    init

---
+++ new-class/src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import Content from './Components/Content';
+import Total from './Components/Total';
+import Header from './Components/Header';
+
+
+function App() {
+
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    
+    <div>
+
+
+         <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p> Number of exercises {exercises1 + exercises2 + exercises3} </p>
+    </div>
+
+      <Header course={course} />
+
+      <Content part1={ exercises1 } part2={ exercises2 } part3={ exercises3 } />
+
+      <Total exercises= { exercises1 + exercises2 + exercises3 } />
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
---
+++ new-class/src/Components/Total.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Total = (props) => {
+    console.log(props)
+   
+
+return <h1> {props.exercises1}  {props.exercises2}  {props.exercises3} </h1>
+
+}
+
+export default Total;
