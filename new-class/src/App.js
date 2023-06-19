61639b467c208916e3c620ab3275585635e2d5aa
Malexgidi <alagbadamalik@gmail.com>
2023-06-19 09:43:52 -0400

    init

--- new-class/src/App.js
+++ new-class/src/App.js
@@ -39,6 +39,8 @@ function App() {
 
       <Total exercises= { exercises1 + exercises2 + exercises3 } />
 
+      <Total exercises= { exercises1 + exercises2 + exercises3 } />
+
     </div>
   );
 }
